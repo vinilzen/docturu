3bf6081e95e88c6b31282219eb8c2dec77adbce9
vinilzen <marchukilya@gmail.com>
2014-12-14 18:28:44 +0100

    add slim template

--- js/main.js
+++ js/main.js
@@ -59,4 +59,23 @@ $(function(){
 			set_map_height();
 		});
 	}
+
+	$('#accordion .panel-collapse')
+		.on('show.bs.collapse', function(e){
+			if ($(e.target).hasClass('lvl1')) {
+				$(e.target).closest('.panel').addClass('open');
+			}
+		})
+		.on('hide.bs.collapse', function(e){
+			if ($(e.target).hasClass('lvl1')) {
+				$(e.target).closest('.panel').removeClass('open');
+			}
+		});
+
+    $("#doctorSpecialitySelectLeft, #doctorMetroSelectLeft, #clinicSpecialitySelectLeft, #clinicMetroSelectLeft").chosen({
+        no_results_text: 'Нет совпадений',
+        allow_single_deselect: true,
+        search_contains: true,
+        width: '100%'
+    });
 });
